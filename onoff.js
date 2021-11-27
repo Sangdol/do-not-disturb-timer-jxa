12d0fcc7fbf840aaccec11852cb754dadc1d28e7
Sanghyun Lee <hammerha@gmail.com>
2021-11-27 20:23:34 +0100

    Add onoff status command

--- onoff.js
+++ onoff.js
@@ -9,15 +9,22 @@ const { isOn, toggleDoNotDisturb } = require('./utils');
   e.g.,
   node onoff.js on
   node onoff.js off
+  node onoff.js status
   `;
 
-  if (args.length == 0 || !['on', 'off'].includes(args[0])) {
+  if (args.length == 0 || !['on', 'off', 'status'].includes(args[0])) {
     console.log(usage);
     process.exit();
   }
 
-  const turnOn = args[0] === 'on';
   const on = await isOn()
+
+  if (args[0] === 'status') {
+    console.log(on);
+    return
+  }
+
+  const turnOn = args[0] === 'on';
   if (on) {
     if (turnOn) {
       console.log('DND is already on.');
